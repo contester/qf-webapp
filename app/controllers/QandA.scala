f02a08fcc0d403e731fd91a4b3442e6de19c51b8
Paul Komkoff <i@stingr.net>
2015-09-07 17:38:35 +0100

    ...

--- app/controllers/QandA.scala
+++ app/controllers/QandA.scala
@@ -2,12 +2,15 @@ package controllers
 
 import javax.inject.Inject
 
+import akka.actor.{ActorSystem, Props}
+import com.spingo.op_rabbit.{RabbitControl, QueueMessage}
 import jp.t2v.lab.play2.auth.AuthElement
 import models.{AuthConfigImpl, LoggedInTeam, Monitor, Problems}
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.db.slick.DatabaseConfigProvider
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.Json
 import play.api.mvc.{Controller, RequestHeader}
 import slick.driver.JdbcProfile
 import views.html
@@ -17,14 +20,23 @@ import scala.concurrent.{ExecutionContext, Future}
 
 case class ClarificationReqData(problem: String, text: String)
 
+case class ClarificationRequestId(id: Int)
+object ClarificationRequestId {
+  implicit val format = Json.format[ClarificationRequestId]
+}
+
 class QandA @Inject() (dbConfigProvider: DatabaseConfigProvider,
                              monitorModel: Monitor,
                              val auth: AuthWrapper,
+                       system: ActorSystem,
                              val messagesApi: MessagesApi) extends Controller with AuthElement with AuthConfigImpl with I18nSupport {
 
   private val dbConfig = dbConfigProvider.get[JdbcProfile]
   private val db = dbConfig.db
   import dbConfig.driver.api._
+
+  val rabbitMq = system.actorOf(Props[RabbitControl])
+  import com.spingo.op_rabbit.PlayJsonSupport._
 
   private def anyUser(account: LoggedInTeam): Future[Boolean] = Future.successful(true)
 
@@ -54,11 +66,14 @@ class QandA @Inject() (dbConfigProvider: DatabaseConfigProvider,
       formWithErrors => clrForm(loggedIn, formWithErrors).map(BadRequest(_)),
       clrData => {
         db.run(
-          sqlu"""insert into ClarificationRequests (Contest, Team, Problem, Request, Arrived) values
+          (sqlu"""insert into ClarificationRequests (Contest, Team, Problem, Request, Arrived) values
                 (${loggedInTeam.contest.id}, ${loggedInTeam.team.localId}, ${clrData.problem}, ${clrData.text},
                 CURRENT_TIMESTAMP())
-              """
-        ).map { _ =>
+              """.andThen(sql"select last_insert_id()".as[Int])).withPinnedSession
+        ).map { clrIds =>
+          clrIds.foreach { clrId =>
+            rabbitMq ! QueueMessage(ClarificationRequestId(clrId), queue = "contester.clarificationrequests")
+          }
           Redirect(routes.QandA.index)
         }
       }
