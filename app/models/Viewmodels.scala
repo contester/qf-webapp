b8c3c86a73aad456a3f4015dcce70df27730db64
Paul Komkoff <i@stingr.net>
2015-10-06 00:49:15 +0300

    ..

--- app/models/Viewmodels.scala
+++ app/models/Viewmodels.scala
@@ -7,3 +7,10 @@ object Compressor {
   compressor.setRemoveSurroundingSpaces("html,head,body,br,p,h1,h2,h3,h4,h5,h6,blockquote,center,dl,fieldset,form,frame,frameset,hr,noframes,ol,table,tbody,tr,td,th,tfoot,thead,ul")
   def apply(html: String) = compressor.compress(html)
 }
+
+object LocalDateFormat {
+  import com.github.nscala_time.time.StaticDateTimeFormat
+  private val fmt = StaticDateTimeFormat.forPattern("yyyy-MM-dd hh:mm:ss")
+  import com.github.nscala_time.time.Imports._
+  def apply(x: DateTime) = fmt.print(x)
+}
