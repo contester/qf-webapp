db3a6031b6ba70dd2bcc129ec8595d852044e326
Paul Komkoff <i@stingr.net>
2015-10-09 23:34:55 +0300

    Fixed my stupid javascripts.

--- public/js/local.js
+++ public/js/local.js
@@ -52,78 +52,79 @@ function notifyMe(n_title, n_icon, n_body) {
     var clrState = {};
     var pingState = {};
 
-    function startIntervalUpdate() {
-        countdownState.interval = setInterval(function() { intervalUpdateContestTimes() }, 1000);
+    function startIntervalUpdate(iconbase, admin) {
+        if (!countdownState.interval)
+            countdownState.interval = setInterval(function() { intervalUpdateContestTimes(iconbase, admin) }, 1000);
     }
 
-    function updateContestTimes(e, iconbase) {
-        if (countdownState.interval) {
-            clearInterval(countdownState.interval);
-            countdownState.interval = null;
+    function updateContestState(e) {
+        var now = Date.now();
+        if (e.startTime > now) {
+            e.state = "before"
+            e.flipMs = e.startTime - now
+        } else if (e.endTime > now) {
+            e.state = "running"
+            e.flipMs = e.endTime - now
+        } else {
+            e.state = "finished"
+            e.flipMs = null
         }
+    }
 
-        if (countdownState.initialOffset) {
-            clearTimeout(countdownState.initialOffset);
-            countdownState.initialOffset = null;
-        }
+    function updateContestTimes(e, iconbase, admin) {
+        e.norm = Date.now()
+        e.startTime = new Date(e.norm + e.startTimeDelta)
+        e.endTime = new Date(e.norm + e.endTimeDelta)
 
-        e.started = e.startTimeDelta <= 0;
-
-        if (countdownState.contest && !countdownState.contest.started && e.started) {
-            notifyMe(e.name, iconbase, 'Соревнование началось');
-            location.reload();
-        }
-
+        e.prevState = countdownState.contest ? countdownState.contest.state : null;
         countdownState.contest = e;
 
-        if (!e.started) {
-            e.timeval = e.startTimeDelta
-        } else if (e.freezeTimeDelta > 0 && e.freezeTimeDelta < e.endTimeDelta) {
-            e.timeval = e.freezeTimeDelta
-        } else if (e.endTimeDelta > 0) {
-            e.timeval = e.endTimeDelta
-        } else if (e.exposeTimeDelta > 0) {
-            e.timeval = exposeTimeDelta
-        } else {
-            e.timeval = 0
-        }
-
-        if (e.timeval != 0) {
-            e.flip = new Date(Date.now() + e.timeval);
-        }
-
-        intervalUpdateContestTimes();
-
-        if (e.timeval != 0) {
-            offset = (e.flip - Date.now()) % 1000;
-            if (offset > 10) {
-                countdownState.initialOffset = setTimeout(function() { startIntervalUpdate() }, offset - 1);
-            } else {
-                startIntervalUpdate()
-            }
-        }
+        intervalUpdateContestTimes(iconbase, admin);
     }
 
-    function intervalUpdateContestTimes() {
-        if (countdownState.contest.started) {
-            $("#contest-starting-in").hide();
-            $("#contest-ending-in").show();
-        } else {
-            $("#contest-starting-in").show();
-            $("#contest-ending-in").hide();
+    function intervalUpdateContestTimes(iconbase, admin) {
+        updateContestState(countdownState.contest)
+        if (countdownState.contest.prevState != countdownState.contest.state) {
+            var notify = null;
+            switch (countdownState.contest.state) {
+                case "before":
+                    $("#contest-timer").show();
+                    $("#contest-ended").hide();
+                    $("#contest-starting-in").show();
+                    $("#contest-ending-in").hide();
+                    startIntervalUpdate(iconbase, admin);
+                    break;
+                case "running":
+                    notify = "началось";
+                    $("#contest-timer").show();
+                    $("#contest-ended").hide();
+                    $("#contest-starting-in").hide();
+                    $("#contest-ending-in").show();
+                    startIntervalUpdate(iconbase, admin);
+                    break;
+                case "finished":
+                    notify = "закончилось"
+                    $("#contest-timer").hide();
+                    $("#contest-ended").show();
+                    $("#contest-starting-in").hide();
+                    $("#contest-ending-in").hide();
+                    if (countdownState.interval) {
+                        clearInterval(countdownState.interval);
+                        countdownState.interval = null;
+                    }
+            }
+            if (countdownState.contest.prevState && notify) {
+                notifyMe(countdownState.contest.name, iconbase, "Соревнование " + notify);
+            }
+            if (countdownState.contest.prevState == "before" && countdownState.contest.state == "running" && !admin) {
+                location.reload();
+            }
         }
 
-        if (countdownState.contest.ended) {
-            $("contest-ended").show();
-            $("contest-timer").hide();
-        } else {
-            $("contest-ended").hide();
-            $("contest-timer").show();
+        if (countdownState.contest.flipMs) {
+            $("#contest-timer-counter").text(formatSeconds((countdownState.contest.flipMs /  1000) >> 0));
         }
-
-        if (countdownState.contest.flip) {
-            $("#contest-timer-counter").text(formatSeconds(((countdownState.contest.flip - Date.now()) /  1000) >> 0));
-        }
+        countdownState.contest.prevState = countdownState.contest.state
     }
 
 function listenOnEvents(path, iconbase, ackMessagePath) {
@@ -163,7 +164,6 @@ function listenOnEvents(path, iconbase, ackMessagePath) {
             if (obj.result.success) {
                 icon = iconbase + 'baloons/baloon-' + obj.problem.toLowerCase() + '.png';
             }
-            console.log(obj.submitId)
             notifyMe("Задача " + obj.problem, icon, obj.result.message)
             if (obj.msgid) {
                 $.post(ackMessagePath, {'msgid': obj.msgid});
@@ -193,7 +193,6 @@ function listenOnEvents(path, iconbase, ackMessagePath) {
 
     source.addEventListener('clarificationPosted', function(ev) {
         var obj = JSON.parse(ev.data);
-        console.log(obj);
         var clrp = $("#clrPending");
         clrp.text("!");
         clrp.show();
@@ -216,8 +215,7 @@ function listenOnEvents(path, iconbase, ackMessagePath) {
     source.onerror = function(ev) {
         $("#connected1").addClass("badge-error");
         $("#connected1").text("!");
-        console.log("Error")
-        console.log(ev)
+        console.log("Error: " + ev)
         if (pingState && pingState.tm) {
             clearTimeout(pingState.tm);
             pingState.tm = null;
@@ -278,7 +276,7 @@ function listenOnAdmin(path, iconbase) {
 
     source.addEventListener('contest', function(ev) {
         var obj = JSON.parse(ev.data);
-        updateContestTimes(obj, iconbase);
+        updateContestTimes(obj, iconbase, true);
     })
 
     source.addEventListener('ping', function(ev) {
@@ -288,8 +286,7 @@ function listenOnAdmin(path, iconbase) {
     source.onerror = function(ev) {
         $("#connected1").addClass("badge-error");
         $("#connected1").text("!");
-        console.log("Error")
-        console.log(ev)
+        console.log("Error: " + ev)
         if (pingState && pingState.tm) {
             clearTimeout(pingState.tm);
             pingState.tm = null;
