6a6d03a72d55579c68f9f941c0621dff42a10ff7
Paul Komkoff <i@stingr.net>
2016-09-14 12:18:35 +0200

    ...

--- app/actors/WaiterActor.scala
+++ app/actors/WaiterActor.scala
@@ -1,8 +1,14 @@
 package actors
 
-import akka.actor.Actor
-import models.StoredWaiterTask
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, Stash}
+import models.{StoredWaiterTask, WaiterModel, WaiterTaskMessage}
+import play.api.libs.EventSource.{EventDataExtractor, EventNameExtractor}
+import play.api.libs.iteratee.Concurrent
+import play.api.libs.json.Json
 import slick.jdbc.JdbcBackend
+
+import scala.collection.mutable
 
 // Spectator has a list of rooms it cares for.
 // Task goes to the room/list of rooms.
@@ -14,14 +20,76 @@ import slick.jdbc.JdbcBackend
 // - Task updated (same)
 // - Task deleted (id)
 
+import com.github.nscala_time.time.Imports.DateTime
+
 object WaiterActor {
+  case object Load
+  case class Loaded(tasks: List[StoredWaiterTask])
+
+  case class NewTask(message: String, rooms: Set[String])
+  case class AckTask(id: Int, rooms: Set[String])
+  case class UnackTask(id: Int, rooms: Set[String])
+  case class DeleteTask(id: Int)
+
+  case class TaskAcked(id: Int, when: DateTime, rooms: Set[String])
+  case class TaskDeleted(id: Int) extends WaiterTaskMessage
+
+  object TaskDeleted {
+    implicit val format = Json.format[TaskDeleted]
+    implicit val eventNameExtractor = EventNameExtractor[TaskDeleted](_ => Some("waiterTaskDeleted"))
+    implicit val eventDataExtractor = EventDataExtractor[TaskDeleted] { state =>
+      Json.stringify(Json.toJson(state))
+    }
+  }
 }
 
 
-class WaiterActor(db: JdbcBackend#DatabaseDef) extends Actor {
-  val tasks = Map[Int, StoredWaiterTask]()
+class WaiterActor(db: JdbcBackend#DatabaseDef) extends Actor with Stash {
+  val tasks = mutable.Map[Int, StoredWaiterTask]()
+
+  import WaiterActor._
+
+  private val (waiterOut, waiterChannel) = Concurrent.broadcast[WaiterTaskMessage]
 
 
+  override def receive: Receive = {
+    case Load => WaiterModel.load(db).onSuccess {
+      case loaded => self ! Loaded(loaded)
+    }
 
-  override def receive: Receive = ???
+    case Loaded(newtasks) => {
+      newtasks.foreach { task =>
+        tasks.put(task.id, task)
+      }
+      context.become(initialized)
+    }
+  }
+
+  def initialized: Receive = {
+    case NewTask(message, rooms) =>
+      WaiterModel.addNewTask(db, message, rooms).map { s =>
+        self ! s
+        sender ! s
+      }
+
+    case task: StoredWaiterTask => {
+      tasks.put(task.id, task)
+      waiterChannel.push(task)
+    }
+
+    case AckTask(id, rooms) =>
+      WaiterModel.markDone(db, id, rooms.head).onSuccess {
+        case (when) => self ! TaskAcked(id, when, rooms)
+      }
+
+    case TaskAcked(id, when, rooms) => {
+      tasks.get(id).foreach { stored =>
+        val newMsg = StoredWaiterTask(stored.id, stored.when, stored.message, stored.roomsActive, stored.roomsAcked.union(rooms))
+        tasks.put(id, newMsg)
+        waiterChannel.push(newMsg)
+      }
+    }
+
+
+  }
 }
--- app/models/WaiterTasks.scala
+++ app/models/WaiterTasks.scala
@@ -4,14 +4,28 @@ import slick.jdbc.JdbcBackend
 
 import scala.concurrent.{ExecutionContext, Future}
 import com.github.nscala_time.time.Imports.DateTime
+import play.api.libs.EventSource.{EventDataExtractor, EventNameExtractor}
+import play.api.libs.json.Json
+
+abstract trait WaiterTaskMessage {
+  def id: Int
+}
 
 case class StoredWaiterTask(id: Int, when: com.github.nscala_time.time.Imports.DateTime,
-                            message: String, roomsActive: Set[String], roomsAcked: Set[String])
+                            message: String, roomsActive: Set[String], roomsAcked: Set[String]) extends WaiterTaskMessage
+
+object StoredWaiterTask {
+  implicit val format = Json.format[StoredWaiterTask]
+  implicit val eventNameExtractor = EventNameExtractor[StoredWaiterTask](_ => Some("waiterTask"))
+  implicit val eventDataExtractor = EventDataExtractor[StoredWaiterTask] { state =>
+    Json.stringify(Json.toJson(state))
+  }
+}
 
 object WaiterModel {
   import slick.driver.MySQLDriver.api._
 
-  def maybeMakeRooms(db: JdbcBackend#DatabaseDef, roomsActive: List[String])(implicit ec: ExecutionContext): Future[List[String]] = {
+  def maybeMakeRooms(db: JdbcBackend#DatabaseDef, roomsActive: Set[String])(implicit ec: ExecutionContext): Future[Set[String]] = {
     if (roomsActive.isEmpty) {
       makeRooms(db)
     } else Future.successful(roomsActive)
@@ -21,11 +35,14 @@ object WaiterModel {
     db.run(sql"""select Name from Areas""".as[String]).map(_.toList)
   }
 
-  def addNewTask(db: JdbcBackend#DatabaseDef, message: String, roomsActive: List[String])(implicit ec: ExecutionContext): Future[Int] =
+  def addNewTask(db: JdbcBackend#DatabaseDef, message: String, roomsActive: Set[String])(implicit ec: ExecutionContext): Future[StoredWaiterTask] =
   maybeMakeRooms(db, roomsActive).flatMap { newRa =>
     val ra = newRa.mkString(",")
-    db.run(sqlu"""insert into WaiterTasks (Message, Rooms) values ($message, $ra)
-                  """.andThen(sql"select last_insert_id()".as[Int]).withPinnedSession).map(_.headOption.get)
+    val now = DateTime.now
+    db.run(sqlu"""insert into WaiterTasks (TS, Message, Rooms) values ($now, $message, $ra)
+                  """.andThen(sql"select last_insert_id()".as[Int]).withPinnedSession).map(_.headOption.get).map { lastInsertId =>
+      StoredWaiterTask(lastInsertId, now, message, newRa.toSet, Set.empty)
+    }
   }
 
   def markDone(db: JdbcBackend#DatabaseDef, id: Int, room: String)(implicit ec: ExecutionContext): Future[com.github.nscala_time.time.Imports.DateTime] = {
