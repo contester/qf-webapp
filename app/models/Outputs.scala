0885f755ffcd20497351711bc41a828a9cad115e
Paul Komkoff <i@stingr.net>
2016-10-16 19:58:45 +0100

    Trying to fix this gridfs mess.

--- app/models/Outputs.scala
+++ app/models/Outputs.scala
@@ -1,6 +1,5 @@
 package models
 
-import akka.stream.Materializer
 import play.api.libs.ws.WSClient
 import utils.{GridfsContent, GridfsTools, PolygonProblemHandle}
 
@@ -10,22 +9,22 @@ case class ResultAssets(test: Int, input: Option[GridfsContent], output: Option[
 
 object Outputs {
   import Contexts.gridfsExecutionContext
-  def getOutput(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, test: Int)(implicit mat: Materializer): Future[Option[GridfsContent]] =
+  def getOutput(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, test: Int): Future[Option[GridfsContent]] =
     GridfsTools.getFile(ws, s"${prefix}submit/${shortName}/${submitId}/${testingId}/$test/output", 1024)
 
-  def getInput(ws: WSClient, prefix: String, handle: PolygonProblemHandle, test: Int)(implicit mat: Materializer): Future[Option[GridfsContent]] =
+  def getInput(ws: WSClient, prefix: String, handle: PolygonProblemHandle, test: Int): Future[Option[GridfsContent]] =
     GridfsTools.getFile(ws, prefix + handle.inputName(test), 1024)
 
-  def getAnswer(ws: WSClient, prefix: String, handle: PolygonProblemHandle, test: Int)(implicit mat: Materializer): Future[Option[GridfsContent]] =
+  def getAnswer(ws: WSClient, prefix: String, handle: PolygonProblemHandle, test: Int): Future[Option[GridfsContent]] =
     GridfsTools.getFile(ws, prefix + handle.answerName(test), 1024)
 
-  def getAssets(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, test: Int, handle: PolygonProblemHandle)(implicit mat: Materializer) = {
+  def getAssets(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, test: Int, handle: PolygonProblemHandle) = {
     getOutput(ws, prefix, shortName, submitId, testingId, test).zip(getInput(ws, prefix, handle, test)).zip(getAnswer(ws, prefix, handle, test)).map {
       case ((optOutput, optInput), optAnswer) => ResultAssets(test, optInput, optOutput, optAnswer)
     }
   }
 
-  def getAllAssets(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, tests: Seq[Int], handle: PolygonProblemHandle)(implicit mat: Materializer) = {
+  def getAllAssets(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, tests: Seq[Int], handle: PolygonProblemHandle) = {
     Future.sequence(tests.map(test => getAssets(ws, prefix, shortName, submitId, testingId, test, handle))).map(_.map(r => r.test -> r).toMap)
   }
 }
--- app/utils/GridfsTools.scala
+++ app/utils/GridfsTools.scala
@@ -8,15 +8,11 @@ import play.api.Logger
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
 
-case class GridfsContent(content: Array[Byte], truncated: Boolean) {
-  def asString(charset: Charset) = new String(content, charset)
-
-  override def toString: String = asString(GridfsContent.CP1251) + (if (truncated) "\n..." else "")
-}
-
-object GridfsContent {
-  val CP1251 = Charsets.toCharset("CP1251")
+case class GridfsContent(content: String, truncated: Boolean, size: Option[Long]) {
+  def sizeStr = size.map(x => s"($x)").getOrElse("")
+  def truncStr = if (truncated) "[обрезан]" else None
 }
 
 object GridfsTools {
@@ -26,7 +22,8 @@ object GridfsTools {
         resp.status match {
         case 200 =>
           val truncated = resp.header("X-Fs-Truncated").map(_ == "true").getOrElse(false)
-          Some(GridfsContent(resp.bodyAsBytes.toArray, truncated))
+          val origSize = resp.header("X-Fs-Content-Length").flatMap(x => Try(x.toLong).toOption)
+          Some(GridfsContent(resp.body, truncated, origSize))
         case _ =>
           Logger.info(s"getFile($name): ${resp.status} ${resp.statusText}")
           None
