711cac4c314d6e4a94c33bbc4d52329661c20502
Paul Komkoff <i@stingr.net>
2017-06-29 23:57:47 +0100

    ...

--- app/actors/ContestStateActor.scala
+++ app/actors/ContestStateActor.scala
@@ -1,8 +1,9 @@
 package org.stingray.qf.actors
 
 import akka.actor.Props
+import com.github.nscala_time.time.Imports.DateTime
 import models.Contest
-import slick.jdbc.JdbcBackend
+import slick.jdbc.{GetResult, JdbcBackend}
 
 import scala.concurrent.Future
 
@@ -12,7 +13,7 @@ object ContestStateActor {
   def props(db: JdbcBackend#DatabaseDef) = Props(new TeamStateActor(db))
 }
 
-class TeamStateActor(db: JdbcBackend#DatabaseDef) extends AnyStateActor[ContestStateActor.ContestState] {
+class ContestStateActor(db: JdbcBackend#DatabaseDef) extends AnyStateActor[ContestStateActor.ContestState] {
   import AnyStateActor._
   import ContestStateActor._
   import context.dispatcher
@@ -21,13 +22,17 @@ class TeamStateActor(db: JdbcBackend#DatabaseDef) extends AnyStateActor[ContestS
 
   import slick.jdbc.MySQLProfile.api._
 
-  private val dbioCombined =
-    sql"""select ID, Name from Schools""".as[(Int, String)].zip(
-      sql"""select ID, School, Num, Name from Teams""".as[(Int, Int, Int, String)]
-    ).zip(sql"""select Contest, Team, LocalID, Disabled, NoPrint, NotRated from Participants"""
-      .as[(Int, Int, Int, Boolean, Boolean, Boolean)])
+  implicit val convertContests = GetResult(r => Contest(r.nextInt(), r.nextString(), r.nextBoolean(),
+    new DateTime(r.nextTimestamp()), new DateTime(r.nextTimestamp()), new DateTime(r.nextTimestamp()),
+    new DateTime(r.nextTimestamp())))
 
-  override def loadStart(): Future[ContestState] = ???
+  private val getContests =
+    sql"""select ID, Name, SchoolMode, Start, End, Finish, Expose from Contests""".as[Contest]
+
+  override def loadStart(): Future[ContestState] =
+    db.run(getContests).map { rows =>
+      rows.map(x => x.id -> x).toMap
+    }
 
   override def setState(v: ContestState): Unit = {
     contests = v
