a81a8e7b4187c73377f908298190b517b077b127
Paul Komkoff <i@stingr.net>
2017-06-30 14:40:29 +0100

    ...

--- app/models/Monitor2.scala
+++ app/models/Monitor2.scala
@@ -1,5 +1,7 @@
 package org.stingray.qf.models.monitor
 
+import spire.math.Rational
+
 trait Submit {
   def id: Int
 }
@@ -7,6 +9,8 @@ trait Submit {
 trait AbstractCell
 
 trait Cell[ScoreType <: AbstractScore] extends AbstractCell {
+  def score: ScoreType
+
   def withSubmit(s: Submit): Cell[ScoreType]
   def withoutSubmit(id: Int): Cell[ScoreType]
 }
@@ -19,3 +23,13 @@ trait AbstractRow {
 }
 
 case class ConcreteRow[ScoreType <: AbstractScore](team: Int, score: ScoreType, cells: Map[String, Cell[ScoreType]]) extends AbstractRow
+
+case class SchoolScore(val r: Rational) extends AbstractScore
+
+case class SchoolCell(base: Int, submits: Seq[Submit], score: SchoolScore) extends Cell[SchoolScore] {
+  override def withSubmit(s: Submit): Cell[SchoolScore] = {
+    ???
+  }
+
+  override def withoutSubmit(id: Int): Cell[SchoolScore] = ???
+}
