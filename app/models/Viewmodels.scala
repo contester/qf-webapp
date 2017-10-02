eb869aaa088bd419e80487f9e274e501a885cbed
Paul Komkoff <i@stingr.net>
2017-10-02 17:25:26 +0100

    ...

--- app/models/Viewmodels.scala
+++ app/models/Viewmodels.scala
@@ -3,8 +3,9 @@ package models
 import com.googlecode.htmlcompressor.compressor.HtmlCompressor
 
 object Compressor {
-  val compressor = new HtmlCompressor()
-  compressor.setRemoveSurroundingSpaces("html,head,body,br,p,h1,h2,h3,h4,h5,h6,blockquote,center,dl,fieldset,form,frame,frameset,hr,noframes,ol,table,tbody,tr,td,th,tfoot,thead,ul")
+  private val compressor = new HtmlCompressor()
+  compressor.setRemoveSurroundingSpaces(
+    "html,head,body,br,p,h1,h2,h3,h4,h5,h6,blockquote,center,dl,fieldset,form,frame,frameset,hr,noframes,ol,table,tbody,tr,td,th,tfoot,thead,ul")
   def apply(html: String) = compressor.compress(html)
 }
 
