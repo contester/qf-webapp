684c077e1fd7a7fdd04bf6b382a5e3dd0f40f82d
Paul Komkoff <i@stingr.net>
2018-10-12 20:50:31 +0400

    ...

--- app/actors/StatusActor.scala
+++ app/actors/StatusActor.scala
@@ -313,6 +313,7 @@ class StatusActor(db: JdbcBackend#DatabaseDef) extends Actor with Stash {
     }
 
     case JoinUser(contest: Int, team: Int) => {
+      Logging.info(s"Join user: $contest, $team")
       val stored = getUnacked(contest, team).map {
         case (msgid, msg) => msg
       }
@@ -333,6 +334,8 @@ class StatusActor(db: JdbcBackend#DatabaseDef) extends Actor with Stash {
         }
         case None => false
       }
+
+      Logging.info(s"unseen: $unseen")
 
       val allSources = Source.combine(
         contestStreamSource(contest) via EventSource.flow,
