c6ff269a855fd59e8eeb1c87fd6c4be1cd09f2e9
Paul Komkoff <i@stingr.net>
2019-08-12 20:51:57 +0100

    Remove old fetch asset code

--- app/models/Outputs.scala
+++ app/models/Outputs.scala
@@ -1,39 +1,17 @@
 package models
 
-import controllers.Assets
 import play.api.Logging
 import play.api.libs.ws.WSClient
 import protos.Assets.{Asset, TestingRecord}
-import utils.GridfsTools.logger
-import utils.{GridfsContent, GridfsTools, PolygonProblemHandle}
+import utils.{GridfsContent, PolygonProblemHandle}
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.concurrent.ExecutionContext
 
 case class ResultAssets(test: Int, input: Option[GridfsContent], output: Option[GridfsContent], answer: Option[GridfsContent])
 
 object Outputs extends Logging {
-  def assetToGc(a: Asset): GridfsContent = {
+  private def assetToGc(a: Asset): GridfsContent = {
     GridfsContent(a.data.toStringUtf8, a.truncated, Some(a.originalSize))
-  }
-
-  def getOutput(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, test: Int)(implicit ec: ExecutionContext): Future[Option[GridfsContent]] =
-    GridfsTools.getFile(ws, s"${prefix}submit/${shortName}/${submitId}/${testingId}/$test/output", 1024)
-
-  def getInput(ws: WSClient, prefix: String, handle: PolygonProblemHandle, test: Int)(implicit ec: ExecutionContext): Future[Option[GridfsContent]] =
-    GridfsTools.getFile(ws, prefix + handle.inputName(test), 1024)
-
-  def getAnswer(ws: WSClient, prefix: String, handle: PolygonProblemHandle, test: Int)(implicit ec: ExecutionContext): Future[Option[GridfsContent]] =
-    GridfsTools.getFile(ws, prefix + handle.answerName(test), 1024)
-
-  def getAssets(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, test: Int, handle: PolygonProblemHandle)(implicit ec: ExecutionContext) = {
-    getOutput(ws, prefix, shortName, submitId, testingId, test).zip(getInput(ws, prefix, handle, test)).zip(getAnswer(ws, prefix, handle, test)).map {
-      case ((optOutput, optInput), optAnswer) => ResultAssets(test, optInput, optOutput, optAnswer)
-    }
-  }
-
-  def getAllAssets(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, tests: Seq[Int], handle: PolygonProblemHandle)(implicit ec: ExecutionContext) = {
-    Future.sequence(tests.map(test => getAssets(ws, prefix, shortName, submitId, testingId, test, handle))).map(_.map(r => r.test -> r).toMap)
   }
 
   def getAllAssets2(ws: WSClient, prefix: String, shortName: String, submitId: Int, testingId: Int, tests: Seq[Int], handle: PolygonProblemHandle)(implicit ec: ExecutionContext) = {
--- app/utils/GridfsTools.scala
+++ app/utils/GridfsTools.scala
@@ -1,31 +1,6 @@
 package utils
 
-import play.api.{Logger, Logging}
-import play.api.libs.ws.WSClient
-
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
-
 case class GridfsContent(content: String, truncated: Boolean, size: Option[Long]) {
   def sizeStr = size.map(x => s"(${x}b)").getOrElse("")
   def truncStr = if (truncated) "[обрезан]" else ""
-}
-
-object GridfsTools extends Logging {
-  def getFile(ws: WSClient, name: String, sizeLimit: Long)(implicit ec: ExecutionContext): Future[Option[GridfsContent]] = {
-    import scala.concurrent.duration._
-    ws.url(name).withRequestTimeout(1.seconds)
-      .withHttpHeaders("X-Fs-Limit" -> sizeLimit.toString).get()
-      .map { resp =>
-        resp.status match {
-        case 200 =>
-          val truncated = resp.header("X-Fs-Truncated").exists(_ == "true")
-          val origSize = resp.header("X-Fs-Content-Length").flatMap(x => Try(x.toLong).toOption)
-          Some(GridfsContent(resp.body, truncated, origSize))
-        case _ =>
-          logger.info(s"getFile($name): ${resp.status} ${resp.statusText}")
-          None
-      }
-    }
-  }
 }
