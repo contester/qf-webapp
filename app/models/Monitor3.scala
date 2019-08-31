587b341d6e0b6941129d4d5dfcbda4187fa80656
Paul Komkoff <i@stingr.net>
2019-08-31 21:04:48 +0100

    ...

--- app/models/Monitor3.scala
+++ app/models/Monitor3.scala
@@ -4,42 +4,29 @@ import org.joda.time.Duration
 
 import scala.collection.{SortedMap, mutable}
 
-sealed trait TestingResultLike
-
-sealed trait ContestTimestampLike
-
-trait IncomingSubmitLike {
-  def id: Long
-  def arrived: ContestTimestampLike
-  def teamID: Long
-  def problemID: String
-  def testingResult: Option[TestingResultLike]
-}
-
 case class Cell3(success: Option[Duration], failedAttempts: Int)
 
 case class MonitorRow3(team: Long, rank: Option[Int], penalty: Long, cells: Map[String, Cell3])
 
-class ProblemSubmits {
-  val tsubmits = mutable.HashMap[Long, IncomingSubmitLike]()
-
-  def update(s: IncomingSubmitLike) = {
-    tsubmits.put(s.id, s)
-  }
+case class ProblemSubmits(data: Map[Int, Submit]) {
+  def update(s: Submit): ProblemSubmits =
+    copy(data = data.updated(s.submitId.id, s))
 }
 
-class TeamSubmits {
-  val tsubmits = mutable.HashMap[String, ProblemSubmits]()
-
-  def update(s: IncomingSubmitLike) = {
-    tsubmits.getOrElseUpdate(s.problemID, new ProblemSubmits).update(s)
-  }
+object ProblemSubmits {
+  val empty = ProblemSubmits(Map.empty)
 }
 
-class MutableMonitor(teams: Map[Long, LocalTeam]) {
-  val tsubmits = mutable.HashMap[Long, TeamSubmits]()
+case class TeamSubmits(data: Map[String, ProblemSubmits]) {
+  def update(s: Submit): TeamSubmits =
+    copy(data = data.updated(s.submitId.problem.id, data.getOrElse(s.submitId.problem.id, ProblemSubmits.empty).update(s)))
+}
 
-  def update(s: IncomingSubmitLike) = {
-    tsubmits.getOrElseUpdate(s.teamID, new TeamSubmits).update(s)
-  }
+object TeamSubmits {
+  val empty = TeamSubmits(Map.empty)
+}
+
+case class ImmutableMonitor(data: Map[Int, TeamSubmits]) {
+  def update(s: Submit): ImmutableMonitor =
+    copy(data = data.updated(s.submitId.teamId, data.getOrElse(s.submitId.teamId, TeamSubmits.empty).update(s)))
 }
