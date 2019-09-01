62c77239ac9d01f1673de3a4fee4e85a59ec7482
Paul Komkoff <i@stingr.net>
2019-09-01 10:32:55 +0100

    ...

--- app/models/Monitor3.scala
+++ app/models/Monitor3.scala
@@ -1,5 +1,6 @@
 package models
 
+import models.Monitor3.CellScore
 import org.joda.time.Duration
 
 import scala.collection.{SortedMap, mutable}
@@ -13,8 +14,34 @@ case class ProblemSubmits(data: Map[Int, Submit]) {
     copy(data = data.updated(s.submitId.id, s))
 }
 
+object Monitor3 {
+  type SubmitScore = Option[Long]
+
+  case class CellScore(success: SubmitScore, failedAttempts: Int)
+}
+
+case class ProblemSubmitsScored(data: Map[Int, Submit], score: Monitor3.CellScore)
+
 object ProblemSubmits {
   val empty = ProblemSubmits(Map.empty)
+
+  def score(data: Map[Int, Submit], submitToScore: Option[Int]): (Monitor3.CellScore, Monitor3.SubmitScore) = {
+    val (failures, successes) = data.values.toSeq.sortBy(_.submitId.arrived).span(!_.success)
+    val success = successes.headOption
+    val successScore: Monitor3.SubmitScore = if(success.isEmpty) { None } else Some(failures.length * 20)
+    val ss = submitToScore.flatMap { sid =>
+      if (success.exists(_.submitId.id == sid)) {
+        successScore
+      } else None
+    }
+    (CellScore(successScore, failures.length), ss)
+  }
+
+  def update(prev: ProblemSubmitsScored, submit: Submit): (ProblemSubmitsScored, Monitor3.SubmitScore) = {
+    val next = prev.data.updated(submit.submitId.id, submit)
+    val (nextCell, submitScore) = score(next, Some(submit.submitId.id))
+    (ProblemSubmitsScored(next, nextCell), submitScore)
+  }
 }
 
 case class TeamSubmits(data: Map[String, ProblemSubmits]) {
