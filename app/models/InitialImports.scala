513cab10cf039a160719c5dd92899b43cc933469
Paul Komkoff <i@stingr.net>
2019-09-11 21:15:59 +0100

    Port team readers

--- app/models/InitialImports.scala
+++ app/models/InitialImports.scala
@@ -1,6 +1,41 @@
 package models
 
 import slick.basic.DatabaseConfig
-import slick.jdbc.JdbcProfile
+import slick.jdbc.{JdbcBackend, JdbcProfile}
+import slick.sql.SqlStreamingAction
+
+import scala.concurrent.{ExecutionContext, Future}
 
 case class ExternalDatabases(studentWeb: DatabaseConfig[JdbcProfile], schoolWeb: DatabaseConfig[JdbcProfile])
+
+case class ImportedSchool(schoolName: String, schoolFullName: String)
+
+case class ImportedTeam(school: ImportedSchool, teamID: Int, teamName: String)
+
+object InitialImportTools {
+  import slick.jdbc.MySQLProfile.api._
+  import utils.Db._
+  import com.github.tototoshi.slick.MySQLJodaSupport._
+
+  private def pickName(s: String) =
+    s.split(' ').head
+
+  private def pickNames(n1: String, n2: String, n3: String) =
+    s"${pickName(n1)}, ${pickName(n2)}, ${pickName(n3)}"
+
+  def getImportedTeamsEn(db: JdbcBackend#DatabaseDef)(implicit ec: ExecutionContext): Future[Iterable[ImportedTeam]] = {
+    db.run(
+      sql"""select t.contestant1_name_en, t.contestant2_name_en, t.contestant3_name_en, u.full_name_en,
+           u.short_name_en from universities u, teams t where u.un_id=t.un_idf AND u.is_deleted='0'
+           AND t.is_deleted='0' ORDER BY u.full_name, t.team_id""".as[(String, String, String, String, String)]).map(convertImportedTeams(_))
+  }
+
+  def getImportedTeamsRu(db: JdbcBackend#DatabaseDef)(implicit ec: ExecutionContext): Future[Iterable[ImportedTeam]] = {
+    db.run(
+      sql"""select t.contestant1_name, t.contestant2_name, t.contestant3_name, u.full_name,
+           u.short_name from universities u, teams t where u.un_id=t.un_idf AND u.is_deleted='0'
+           AND t.is_deleted='0' ORDER BY u.full_name, t.team_id""".as[(String, String, String, String, String)]).map(convertImportedTeams(_))
+  }
+  private def convertImportedTeams(m: Iterable[(String, String, String, String, String)]): Iterable[ImportedTeam] =
+    m.groupBy(x => (x._5, x._4)).mapValues(_.zipWithIndex.map(x => ImportedTeam(ImportedSchool(x._1._5, x._1._4), x._2, pickNames(x._1._1, x._1._2, x._1._3)))).values.flatten
+}
